7cde5b10113766d7e63482ad091ebbc196ade7a0
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-03-22 00:47:34 +0900

    inprementation of done method

--- app/assets/javascripts/tweet.js
+++ app/assets/javascripts/tweet.js
@@ -1,22 +1,22 @@
 $(function(){
-  // function buildHTML(tweet){
-  //   var html = `
-  //     <div class="main-tweet__contents__content__block">
-  //       <div class="main-tweet__contents__content__block--text">
-  //         ${tweet.content}
-  //       </div>
-  //       <div class="main-tweet__contents__content__block__rightBox">
-  //         <div class="main-tweet__contents__content__block__rightBox--name">
-  //           ${tweet.name}
-  //         </div>
-  //         <div class="main-tweet__contents__content__block__rightBox--postDate">
-  //           ${tweet.created_at}
-  //         </div>
-  //       </div>
-  //     </div>
-  //   `
-  //   return html;
-  // }
+  function buildHTML(tweet){
+    var html = `
+      <div class="main-tweet__contents__content__block">
+        <div class="main-tweet__contents__content__block--text">
+          ${tweet.content}
+        </div>
+        <div class="main-tweet__contents__content__block__rightBox">
+          <div class="main-tweet__contents__content__block__rightBox--name">
+            ${tweet.name}
+          </div>
+          <div class="main-tweet__contents__content__block__rightBox--postDate">
+            ${tweet.created_at}
+          </div>
+        </div>
+      </div>
+    `
+    return html;
+  }
   $('#new_tweet').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -30,11 +30,11 @@ $(function(){
       contentType: false
     })
     .done(function(tweet){
-      console.log(tweet);
-      // var html = buildHTML(tweet);
-      // $('.main-tweet__contents__content').append(html);
-      // $('.main-tweet__contents__messages--text').val('');
-      // $('.main-tweet__contents__messages--btn').prop('disabled', false);
+      var html = buildHTML(tweet);
+      console.log(html);
+      $('.main-tweet__contents__content').append(html);
+      $('.main-tweet__contents__messages--text').val('');
+      $('.main-tweet__contents__messages--btn').prop('disabled', false);
     })
   })
 });
